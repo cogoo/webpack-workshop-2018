42174d4148c552f3fa7793ace8903fcd76a42576
Colin Ogoo <ogoo.colin@gmail.com>
2018-08-07 11:36:38 +0100

    dynamic lazy loading

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import nav from "./nav";
+// import * as gsap from "gsap";
+const getGsap = () => import("gsap");
 // import { footer } from "./footer";
 const getFooter = () => import("./footer");
 import makeButton from "./button";
@@ -8,6 +10,8 @@ import imageUrl from "./webpack-logo.jpg";
 import css from "./footer.css";
 import buttonStyles from "./button.css";
 
+const setButtonStyle = (color) => import(`./button-styles/${color}`);
+
 const image = makeImage(imageUrl);
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
@@ -15,9 +19,17 @@ button.style = makeColorStyle("cyan");
 document.body.appendChild(button);
 
 button.addEventListener("click", e => {
-    getFooter().then(footerModule => {
-        document.body.appendChild(footerModule.footer);
-    });
+	getFooter().then(footerModule => {
+		document.body.appendChild(footerModule.footer);
+	});
+
+	getGsap().then((gsapModule) => {
+		console.log(gsapModule);
+	})
+
+	setButtonStyle('red').then((style) => {
+		button.style = style.default;
+	})
 });
 
 
